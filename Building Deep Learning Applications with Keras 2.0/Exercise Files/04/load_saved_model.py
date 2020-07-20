b54e5a6e30c780ee38780e79b917ba1c26b6429b
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2020-07-20 12:06:16 +0300

    Solved

--- Building Deep Learning Applications with Keras 2.0/Exercise Files/04/load_saved_model.py
+++ Building Deep Learning Applications with Keras 2.0/Exercise Files/04/load_saved_model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from keras.models import load_model
 
-model =
+model = load_model('trained_model.h5')
 
 X = pd.read_csv("proposed_new_product.csv").values
 prediction = model.predict(X)
@@ -15,4 +15,3 @@ prediction = prediction + 0.1159
 prediction = prediction / 0.0000036968
 
 print("Earnings Prediction for Proposed Product - ${}".format(prediction))
-
