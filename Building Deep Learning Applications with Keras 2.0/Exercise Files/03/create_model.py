ff32b84a3b93a852dc5d23d6998cd7ca1a344703
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2020-07-20 12:05:47 +0300

    Solved

--- Building Deep Learning Applications with Keras 2.0/Exercise Files/03/create_model.py
+++ Building Deep Learning Applications with Keras 2.0/Exercise Files/03/create_model.py
@@ -8,4 +8,11 @@ X = training_data_df.drop('total_earnings', axis=1).values
 Y = training_data_df[['total_earnings']].values
 
 # Define the model
-model =
+model = Sequential()
+
+model.add(Dense(50, input_dim=9, activation='relu'))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(1))
+
+model.compile(loss='mean_squared_error', optimizer='adam')
