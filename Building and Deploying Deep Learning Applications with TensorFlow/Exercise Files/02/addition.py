3986deada1e0e83b5adf83bc9548105b298c96ed
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2020-07-28 16:18:52 +0300

    Update addition.py

--- Building and Deploying Deep Learning Applications with TensorFlow/Exercise Files/02/addition.py
+++ Building and Deploying Deep Learning Applications with TensorFlow/Exercise Files/02/addition.py
@@ -5,15 +5,13 @@ import tensorflow as tf
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # Define computational graph
-X =
-Y =
+X = tf.placeholder(tf.float32, name='X')
+Y = tf.placeholder(tf.float32, name='Y')
 
-addition =
-
+addition = tf.add(X, Y, name='addition')
 
 # Create the session
 with tf.Session() as session:
 
-    result =
-
+    result = session.run(addition, feed_dict={X: [1], Y: [4]})
     print(result)
